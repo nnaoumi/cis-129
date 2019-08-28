54acdf4ab4b35a3c610747cca4e298d823206f9d
nnaoumi <54483673+nnaoumi@users.noreply.github.com>
2019-08-28 17:48:30 -0400

    Add files via upload

---
+++ M1 Assignment/1_M1 Assignment Code.py
@@ -0,0 +1,14 @@
+cubeEdge = input('Please enter the length of any edge of a cube: \n')
+
+# Calculation of surface area for one of the sides of a cube
+areaSquare = int(cubeEdge) ** 2
+
+# Calculation for surface area of a cube
+areaCube = int(cubeEdge) ** 2 * 6
+
+# Calculation for volume of a cube
+volumeCube = int(cubeEdge) ** 3
+
+print('Surface area on one side of cube:', areaSquare)
+print('Surface area of cube:', areaCube)
+print('Volume of cube:', volumeCube)
